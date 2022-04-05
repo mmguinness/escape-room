3c39f12322417ac8fc97d8bd7ad2ddb08673033e
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-04-05 17:17:50 +0100

    code tidy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import "./App.css";
 import { Room } from "./components/Room";
 
 function Game() {
   return (
     <section className="game">
-      
       <Room />
     </section>
+  );
+}
+
+export default Game;
