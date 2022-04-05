42f571055887c3d8abd0920d24fa5bff21f7037a
Charlotte Buchan <cebuc@talktalk.net>
2022-04-05 17:09:55 +0100

    gave thekey button a class id

--- src/components/FirstKey.js
+++ src/components/FirstKey.js
@@ -2,11 +2,9 @@ import {React} from 'react';
 import {useState} from 'react';
 
 import myImage from "./keyimg.png";
-console.log('loooooooookkkkkkkk here')
-console.log(myImage)
 export const FirstKey = () => {
   const [disable, setDisable] = useState(false);
-  return <button disabled={disable} onClick={() => 
+  return <button className="firstKey" disabled={disable} onClick={() => 
    setDisable(true)}><img src={myImage} height="100" width="100"/>Collect Key</button>
 };
 
