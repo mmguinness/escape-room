8dbeb844f7afb62df856ed0fdb67853d00cc3933
Charlotte Buchan <cebuc@talktalk.net>
2022-04-05 16:06:32 +0100

    added key image

--- src/components/FirstKey.js
+++ src/components/FirstKey.js
@@ -1,8 +1,10 @@
-import {React} from 'react'
-import {useState} from 'react'
+import {React} from 'react';
+import {useState} from 'react';
+import myImage from '/Users/charlottebuchan/Projects/week-11-12/escape-room/src/key_img.png';
 export const FirstKey = () => {
   const [disable, setDisable] = useState(false);
-  return <button disabled={disable} onClick={() => setDisable(true)}> Get key</button>
+  return <button disabled={disable} onClick={() => 
+   setDisable(true)}><img src={myImage} />Collect Key</button>
 };
 
 
