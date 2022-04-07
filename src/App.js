4e02fb5d9979436aebfe095bece816a6350aba81
Ben Partington <bdpartington@gmail.com>
2022-04-07 15:54:02 +0100

    Started to create key popup handler

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ import { Room } from "./components/Room";
 
 function Game() {
     const [inventory, setInventory] = useState({ key: false, book: false });
+    const [keyPopup, setKeyPopup] = useState(false);
 
   const pickUpKey = () => {
     setInventory({...inventory, key: true});
+    setKeyPopup(!keyPopup);
   };
+  
   return (
     <section className="game">
       <Room />
       <div>{(inventory.key === false) && <FirstKey pickUpKey={pickUpKey} />}</div>
+      {keyPopup && (
+        <KeyPopupBox 
+          handleCloseBox={setKeyPopup}
+          content={
+            <div>
+              <h3>You picked up a key!</h3>
+            </div>
+          }
+        />
+      )}
     </section>
   );
 }
