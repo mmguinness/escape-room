57a28f79bbeabea0d1353f4dc6d527f70f34a6a3
Ben Partington <bdpartington@gmail.com>
2022-04-07 15:35:49 +0100

    Added css for key, placed it in room

--- src/components/FirstKey.js
+++ src/components/FirstKey.js
@@ -1,9 +1,10 @@
 import { React } from "react";
 import keyImage from "./keyimg.png"
+import "./firstKey.css";
 
 export const FirstKey = (props) => {
   return (
      <img className="firstKey" onClick={props.pickUpKey}
-     src={keyImage} role="button" height="100" width="100" alt=""/>
+     src={keyImage} role="button" alt=""/>
   );
 };
