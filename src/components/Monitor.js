341f5a593ff1e2ae73e9e0c9c1e41a12b9cd6b99
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-04-07 16:36:18 +0100

    inputs are being saved

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -4,6 +4,24 @@ import "./monitor.css";
 
 export const Monitor = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [inputOne, setInputOne] = useState("");
+  const [inputTwo, setInputTwo] = useState("");
+
+  const handleChangeOne = eventOne => {
+    setInputOne(eventOne.target.value.toLowerCase());
+  };
+
+  const handleChangeTwo = eventTwo => {
+    setInputTwo(eventTwo.target.value.toLowerCase());
+  };
+
+  const checkInputs = () => {
+    if (inputOne === "red" && inputTwo === "violets") {
+      return console.log("correct answer");
+    } else {
+      console.log("try again");
+    }
+  };
 
   const toggleMonitorPopup = () => {
     setIsOpen(!isOpen);
@@ -25,16 +43,28 @@ export const Monitor = () => {
             <div>
               <h3>Puzzle</h3>
               <div>
-                <form>
-                  Roses are <input type="text" id="wordOne" name="wordOne" />
-                  <br />
-                  <br />
-                  <input type="text" id="wordOne" name="wordOne" /> are blue
-                </form>{" "}
+                Roses are{" "}
+                <input
+                  className="wordOne"
+                  value={inputOne}
+                  onChange={handleChangeOne}
+                ></input>
+                <br />
+                <br />
+                <input
+                  className="wordTwo"
+                  value={inputTwo}
+                  onChange={handleChangeTwo}
+                ></input>
+                <nbsp /> are blue
+                <br />
                 <br />
                 I love coding <br />
                 <br />
                 You should too!
+                <br />
+                <br />
+                <button onClick={checkInputs}>Submit</button>
               </div>
             </div>
           }
