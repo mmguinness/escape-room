ce68855f2c170960a5c227a45155482d151b7027
Charlotte Buchan <cebuc@talktalk.net>
2022-04-07 14:45:30 +0100

    got front page back and setting up click button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-// import { Room } from "./components/Room";
+import { useState } from "react";
+import { Room } from "./components/Room";
 import {GreetingPage} from "./GreetingPage"
 function Game() {
+  const [page, setPage] = useState('GreetingPage')
   return (
-    <section className="game">
-      <GreetingPage/>
-      {/* <Room /> */}
-    </section>
+    <div className="game">
+      {page === "Room" && <Room/>}
+      {page === "GreetingPage" && <GreetingPage/>}
+     
+    </div>
   );
 }
 
