851d4dcdb49a2c57e96a8ae792e381f545bc8798
Ben Partington <bdpartington@gmail.com>
2022-04-08 16:56:19 +0100

    Updated GreetingPage and start of test writing

--- src/GreetingPage.js
+++ src/GreetingPage.js
@@ -1,13 +1,13 @@
 import './GreetingPage.css';
 export const GreetingPage = (props) => {
   return <div className="greetings">
+    <br/>
     <h1 className="welcome" align="center"> Get Me Out Of Here! </h1>
-    <p className="game-explination">  You have no idea how but you've woken up in a strange room. No idea how or why. 
-    However how you got there is irrelivant, it's how youget out that is the important part. 
-    Following a series of clues, your aim is to escape before the timer runs out.
-    What happens when the timer runs out? Who knows. all you know is 'get me out of here'. </p><br/>
-    <button align="center"className="play-game" onClick={(props.greetingFn) } >Time To Escape!</button>
-
+      <p className="game-explanation">You awake in a strange room, with no memory of how or why. 
+      Using your investigative skills, you must search for clues and solve a series of puzzles to 
+      escape before the timer runs out!
+      </p>
+    <button align="center" className="play-game" onClick={(props.greetingFn)}>Time To Escape!</button>
   </div>
 
 }
