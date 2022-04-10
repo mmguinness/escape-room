19ccfceb029e5ac5bef1b8c1b17ac8814f5f7d7d
Ben Partington <bdpartington@gmail.com>
2022-04-10 11:12:47 +0100

    First test, checking if title appears on greeting page

--- src/GreetingPage.test.js
+++ src/GreetingPage.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import {render, fireEvent, screen} from '@testing-library/react'
-import GreetingPage from './GreetingPage';
+import {render, screen} from '@testing-library/react'
+import {GreetingPage} from './GreetingPage';
 
 
-test('button starts game', () => {
-  render(<GreetingPage/>)
-  fireEvent.click(screen.getByText('Time To Escape!'))
-  expect(screen.getByRole('alert')).toHaveTextContent('Oops, failed to fetch!')
+test('renders title', () => {
+  render(<GreetingPage />);
+  const titleElement = screen.getByText(/Get Me Out Of Here!/i);
+  expect(titleElement).toBeInTheDocument();
 })
